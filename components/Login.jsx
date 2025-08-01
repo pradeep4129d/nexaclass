4e6f9e6b07a9b5b140e95ed531e54664e6d7cf6c
pradeep4129d <bolla.h678@gmail.com>
2025-08-01 19:45:40 +0530

    4

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const Login = () => {
+  const handleSubmit=(e)=>{
+  }
   return (
-    <div>Login</div>
+    <div className='login'>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="email">
+          <input type="text" name='email' id='u' placeholder='Email'/>
+          <ion-icon name="mail"></ion-icon>
+        </div>
+        <div className="password">
+          <input type="password" name="password" id="p" placeholder='password'/>
+          <ion-icon name="key"></ion-icon>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
